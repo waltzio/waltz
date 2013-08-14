6364f46efb60d9616cfc062b414922111508a2dd
Joe Wegner <joe@wegnerdesign.com>
2013-08-14 17:44:16 -0500

    Add clef logo animation

--- js/clef.js
+++ js/clef.js
@@ -1,8 +1,8 @@
 (function($) {
 
-console.log("in");
-
-var clefCircle = $("<div>");
+var clefSource = chrome.extension.getURL("/img/clef128.png");
+var clefCircle = $("<div><img src='"+clefSource+"' height='64' width='64' /></div>");
+var clefImage = $(clefCircle).children("img");
 
 $(clefCircle).css({
 	position: "fixed",
@@ -15,10 +15,18 @@ $(clefCircle).css({
 	"border-color": "rgba(13, 157, 219, .9)",
 	"border-style": "solid",
 	"border-width": 0,
-	"-webkit-transition": "border-width 1.5s, height .5s, width .5s, bottom .5s, right .5s"
+	"-webkit-transition": "border-width 1s, height .7s, width .7s, bottom .7s, right .7s"
+});
+
+$(clefImage).css({
+	display: "none",
+	position: "relative",
+	top: 100,
+	left: 100
 });
 
 $("body").append(clefCircle);
+console.log(clefCircle);
 
 setTimeout(function() {
 	$(clefCircle).css({
@@ -29,7 +37,24 @@ setTimeout(function() {
 		"border-width": 50,
 		"border-radius": 400
 	})
+
+	setTimeout(function() {
+		$(clefCircle).children("img").fadeIn(500);
+	}, 1000);
 }, 500);
+
+setInterval(function() {
+	$(clefImage).css({
+		"-webkit-transition": "none",
+		"-webkit-transform": "rotateX(0deg)"
+	});
+	setTimeout(function() {
+		$(clefImage).css({
+			"-webkit-transition": "-webkit-transform 1s",
+			"-webkit-transform": "rotateX(360deg)"
+		});
+	}, 10)
+}, 3000);
 
 
 })(jQuery);
