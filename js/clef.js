6cecd30d72477dc256aaf40b6837c79cbb8c2a36
Joe Wegner <joe@wegnerdesign.com>
2013-08-16 08:01:51 -0500

    basic SVG spin

--- js/clef.js
+++ js/clef.js
@@ -1,29 +1,23 @@
 (function($) {
 
 var cSource = chrome.extension.getURL("/img/clef128.png");
-console.log(cSource);
-var clefCircle = $("<div><img src='"+cSource+"' width='64' height='64' /></div>");
+var fSource = chrome.extension.getURL("/img/clef-full.png");
+
+var clefCircle = $("<div id='clef-vault-login-wrapper' class='spinning'></div>");
+var clefActions = $(
+	"<svg width='64' height='64'>"
+		+"<g>"
+			+"<path d='M22.5,51.5A16,16,0,1,1,42,14' stroke='#0d9ddb' fill='none' stroke-width='19.5'></path>"
+			+"<path d='M42,14A21.2,21.2,0,0,1,41.5,51.5' stroke='#0d569c' fill='none' stroke-width='19.5'></path>"
+		+"</g>"
+	+"</svg>");
 
 $(clefCircle).css({
-	position: "fixed",
-	bottom: 25,
-	right: 25
-});
+	"background-image": "url("+cSource+")"
+}).append(clefActions);
 
 $("body").append(clefCircle);
 
-setInterval(function() {
-	console.log("flip");
-	$(clefCircle).css({
-		'-webkit-transform': 'rotateZ(0deg)',
-		"-webkit-transition": "none"
-	});
-	setTimeout(function() {
-		$(clefCircle).css({
-			"-webkit-transition": "-webkit-transform 1s",
-			'-webkit-transform': 'rotateZ(360deg)'
-		});
-	}, 10);
-}, 3000);
+
 
 })(jQuery);
