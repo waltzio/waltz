fe8a2d67dd35a92408576358b2a157d8255e34c2
Joe Wegner <joe@wegnerdesign.com>
2013-08-19 17:14:30 -0500

    Update README

--- js/crypto.js
+++ js/crypto.js
@@ -13,6 +13,7 @@ function ClefCrypto() {
 
 ClefCrypto.prototype.encrypt = function(pre, identifier, cb) {
 	setTimeout(function() {
+		//Eventually this will grab the encryption key from the augmenter.  For now, just use the identifer
 		var encrypted = CryptoJS.AES.encrypt(pre, identifier);
 		if(typeof(cb) === "function") {
 			cb(encrypted.toString());
@@ -22,6 +23,7 @@ ClefCrypto.prototype.encrypt = function(pre, identifier, cb) {
 
 ClefCrypto.prototype.decrypt = function(pre, identifer, cb) {
 	setTimeout(function() {
+		//Eventually this will grab the encryption key from the augmenter.  For now, just use the identifer
 		var decrypted = CryptoJS.AES.decrypt(pre, identifer);
 		if(typeof(cb) === "function") {
 			cb(decrypted.toString(CryptoJS.enc.Utf8));
