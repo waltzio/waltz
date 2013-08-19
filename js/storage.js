674b780d9f1590d9240366c1e50a54911b2bceba
Joe Wegner <joe@wegnerdesign.com>
2013-08-19 16:03:37 -0500

    Basic get/pull from local storage

--- js/storage.js
+++ js/storage.js
@@ -12,12 +12,29 @@ function Storage() {
 }
 
 Storage.prototype.getCredentialsForDomain = function(domain, cb) {
-	setTimeout(function() {
-		cb({
-			username: "fauxClef",
-			password: "fauxPassword"
-		});
-	}, 0);
+	if(typeof(cb) !== "function") {
+		return false;
+	}
+
+	chrome.storage.local.get(domain, function(data) {
+		if(typeof(data[domain]) === "object") {
+			cb(data[domain]);
+		} else {
+			cb(false);
+		}
+	});
+}
+
+Storage.prototype.storeCredentialsForDomain = function(domain, username, password, cb) {
+	var creds = {}
+	creds[domain] = {
+		username: username,
+		password: password
+	};
+
+	console.log(creds)
+
+	chrome.storage.local.set(creds, cb);
 }
 
 var storage = new Storage();
