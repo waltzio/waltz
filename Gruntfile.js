9675aad990df90b2dc673172a7000608af78d190
Jesse Pollak <jpollak92@gmail.com>
2013-10-06 17:16:20 -0700

    adds gruntfile and sass, yeah

---
+++ Gruntfile.js
@@ -0,0 +1,29 @@
+var grunt = require('grunt'),
+  http = require('http');
+
+grunt.initConfig({
+  watch: {
+    sass: {
+      files: ['scss/*.scss'],
+      tasks: ['sass'],
+      options: {
+        debounceDelay: 500
+      }
+    }
+  },
+  sass: {                              // Task
+    dist: {                            // Target
+      options: {                       // Target options
+        style: 'expanded'
+      },
+      files: {                         // Dictionary of files
+        'css/clef.css': 'scss/clef.scss'
+      }
+    }
+  }
+});
+
+grunt.loadNpmTasks('grunt-contrib-sass');
+grunt.loadNpmTasks('grunt-contrib-watch');
+
+grunt.registerTask('default', ['watch']);
