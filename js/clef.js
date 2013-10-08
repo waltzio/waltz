39713ed9bdfc11070cf351935a4a49ae698bf079
Joe Wegner <joe@wegnerdesign.com>
2013-10-08 15:58:18 -0500

    spinning buttons

--- js/clef.js
+++ js/clef.js
@@ -335,18 +335,15 @@
 		//Grab image resource URLs from extensions API
 		var cSource = chrome.extension.getURL("/img/clef128.png");
 		var fSource = chrome.extension.getURL("/img/clef-full.png");
+		var pSource = chrome.extension.getURL("/img/pencil.png");
 
 
 		//Build HTML for clef widget
 		var clefCircle = $("<div id='clef-vault-login-wrapper' class='spinning'></div>");
 		var clefActions = $(
-			"<svg width='64' height='64'>"
-				+"<g>"
-					+"<path id='clef-login-hit-target' d='M22.5,51.5A16,16,0,1,1,42,14' stroke='#0d9ddb' fill='none' stroke-width='19.5'></path>"
-					+"<path id='clef-dismiss-hit-target' d='M42,14A21.2,21.2,0,0,1,41.5,51.5' stroke='#b4181d' fill='none' stroke-width='19.5'></path>"
-					+"<circle cx='32' cy='32' r='12' />"
-				+"</g>"
-			+"</svg>");
+			"<button style='background-image:url("+pSource+");' class='clef-button clef-dismiss'></button>"
+			+"<button style='background-image:url("+pSource+");' class='clef-button clef-edit'></button>"
+			);
 
 		//Style the widget with the correct image resource
 		$(clefCircle).css({
@@ -354,41 +351,16 @@
 		}).append(clefActions);
 
 
-		var hitActionTimeout;
-		var curAction;
-
-		//Hover and click interactions:
-		//On hover, the widget should spin to 359 deg (359, so it picks up at 0deg on hover-out)
-		//  It should also fade in the SVG hit targets for logging in and dismissing
-		//  The widget is defaulted to the login target, and doesn't bind the hit targets until the
-		//  spinning animation has finished.  This seems to pretty reliably put the user in login mode at the 
-		//  time of hover
-		//
-		//On click the widget should fade out and do a spinning animation.
-		//  When finished it should be removed from the DOM
-		$(clefCircle).hover(function() {
-			// $(clefCircle).addClass("clef-login-action");
-			curAction = "login";
-		}, function() {
-			clearTimeout(hitActionTimeout);
-
-			$(clefCircle).find('path').unbind('hover');
-			$(clefCircle).removeClass("clef-login-action").removeClass("clef-dismiss-action");
-		});
-
 		$(clefCircle).click(function() {
 			$(this).addClass("remove");
 
-			//If curAction is not 'login' or 'dismiss', then something weird happened.  But let's treat that like dismiss
-			if(curAction === 'login') {
-				self.checkAuthentication(function() {
-					if (self.loginCredentials) {
-						self.decryptAndLogIn();
-					} else {
-						self.requestCredentials();
-					}
-				});
-			}
+			self.checkAuthentication(function() {
+				if (self.loginCredentials) {
+					self.decryptAndLogIn();
+				} else {
+					self.requestCredentials();
+				}
+			});
 
 			setTimeout(function() {
 				$(self).remove();
