e73cd82f8143af71aaa2bf3dda146cbec4380c56
Joe Wegner <joe@wegnerdesign.com>
2013-11-04 20:56:52 -0600

    scrolling sections

--- _site/javascripts/main.js
+++ _site/javascripts/main.js
@@ -1,1 +1,11 @@
-console.log('This would be the main JS file.');
+$(document).ready(function() {
+	$(".circle").on('click', function() {
+		var cls = $(this).attr('class').replace('circle ', '');
+
+		var top = $(".feature-detail."+cls).offset().top;
+
+		$("html, body").animate({
+			scrollTop: top - 50
+		}, 800);
+	});
+});
