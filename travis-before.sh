895af26b0294ca557bc89a162f416efe3f888a8a
Jesse Pollak <jpollak92@gmail.com>
2014-02-18 08:19:50 -0800

    don't build chrome extension in travis

--- travis-before.sh
+++ travis-before.sh
@@ -4,6 +4,6 @@ if [[ $TRAVIS_BRANCH == 'master' ]]
 	then
 	echo "Building Site Configs"
 	grunt build-config
-	echo "Building Chrome Extension"
-	grunt chrome-extension
+	# echo "Building Chrome Extension"
+	# grunt chrome-extension
 fi
