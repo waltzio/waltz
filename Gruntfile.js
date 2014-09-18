94250496d60a6637b9cd0790604c9c2f949e7470
Jesse Pollak <jpollak92@gmail.com>
2014-09-18 11:42:45 -0700

    bumps version to 2.0.11

--- Gruntfile.js
+++ Gruntfile.js
@@ -42,7 +42,7 @@ grunt.initConfig({
   'chrome-extension': {
     options: {
         name: "Waltz",
-        version: "2.0.10",
+        version: "2.0.11",
         id: "obhibkfopclldmnoohabnbimocpgdine",
         chrome: "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome",
         clean: true,
