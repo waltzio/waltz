da381c99cba9a8fbcdb8f62b661352863b3d9438
Jesse Pollak <jpollak92@gmail.com>
2014-11-20 16:22:43 -0800

    update to 2.0.13

--- Gruntfile.js
+++ Gruntfile.js
@@ -42,7 +42,7 @@ grunt.initConfig({
   'chrome-extension': {
     options: {
         name: "Waltz",
-        version: "2.0.12",
+        version: "2.0.13",
         id: "obhibkfopclldmnoohabnbimocpgdine",
         chrome: "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome",
         clean: true,
